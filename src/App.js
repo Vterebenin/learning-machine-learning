1d9981ff751a2459757d9270951305ba3a094cda
Vterebenin <niceserver11@mail.ru>
2019-11-10 16:37:32 +0300

    feat: create mvp

--- src/App.js
+++ src/App.js
@@ -1,26 +1,185 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+import WebCam from 'react-webcam'
+import { Button, Grid } from '@material-ui/core'
+import * as tf from '@tensorflow/tfjs';
+import * as knnClassifier from '@tensorflow-models/knn-classifier';
+import * as mobilenet from '@tensorflow-models/mobilenet'
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.infoTexts = [];
+    this.isTraining = true
+    this.training = -1; // -1 when no class is being trained
+    this.recordSamples = false;
+
+    this.classes = ["Left", "Right"];
+    this.testPrediction = false;
+    this.training = true;
+
+    this.test = this.test.bind(this)
+    this.animate = this.animate.bind(this)
+    this.state = {
+      info: []
+    }
+
+    // Initiate deeplearn.js math and knn classifier objects
+
+  }
+
+  test() {
+    this.testPrediction = true
+  }
+
+  componentDidMount() {
+    this.video = document.getElementById('webCam')
+
+
+    this.loadClassifierAndModel();
+    this.setupButtonEvents();
+  }
+
+  async loadClassifierAndModel() {
+    this.knn = knnClassifier.create();
+    this.mobilenetModule = await mobilenet.load();
+    console.log('model loaded')
+
+    this.start();
+  }
+
+
+  setupButtonEvents() {
+    for (let i = 0; i < 2; i++) {
+      let button = document.getElementsByClassName("button")[i];
+
+      button.onmousedown = () => {
+        this.training = i;
+        this.recordSamples = true;
+      };
+      button.onmouseup = () => (this.training = -1);
+
+      const infoText = " No examples added";
+      this.infoTexts.push(infoText);
+    }
+
+    this.setState({
+      info: this.infoTexts
+    })
+  }
+
+  start() {
+    if (this.timer) {
+      this.stop();
+    }
+    this.timer = requestAnimationFrame(this.animate);
+  }
+
+  stop() {
+    cancelAnimationFrame(this.timer);
+  }
+
+  async animate() {
+    if (this.recordSamples) {
+      // Get image data from video element
+      const image = tf.browser.fromPixels(this.video);
+
+      let logits;
+      // 'conv_preds' is the logits activation of MobileNet.
+      const infer = () => this.mobilenetModule.infer(image, "conv_preds");
+
+      // Train class if one of the buttons is held down
+      if (this.training !== -1) {
+        logits = infer();
+
+        // Add current image to classifier
+        this.knn.addExample(logits, this.training);
+      }
+
+      const numClasses = this.knn.getNumClasses();
+
+      if (this.testPrediction) {
+        this.isTraining = false;
+        if (numClasses > 0) {
+          // If classes have been added run predict
+          logits = infer();
+          const res = await this.knn.predictClass(logits, 10);
+          console.log(res.confidences)
+
+          for (let i = 0; i < 2; i++) {
+            // The number of examples for each class
+            const exampleCount = this.knn.getClassExampleCount();
+
+            if (exampleCount[i] > 0) {
+              this.infoTexts[i] = ` ${
+                exampleCount[i]
+              } examples - ${res.confidences[i] * 100}%`;
+              this.setState({
+                info: this.infoTexts
+              })
+            }
+          }
+        }
+      }
+
+      if (this.isTraining) {
+        // The number of examples for each class
+        const exampleCount = this.knn.getClassExampleCount();
+
+        for (let i = 0; i < 2; i++) {
+          if (exampleCount[i] > 0) {
+            this.infoTexts[i] = ` ${exampleCount[i]} examples`;
+            this.setState({
+              info: this.infoTexts
+            })
+          }
+        }
+      }
+
+      // Dispose image when done
+      image.dispose();
+      if (logits != null) {
+        logits.dispose();
+      }
+    }
+    this.timer = requestAnimationFrame(this.animate);
+  }
+
+  render() {
+
+    return (
+      <div className="App">
+        <header className="App-header">
+          <WebCam id={'webCam'} height={227} width={227}/>
+          <Grid container>
+            <Grid item xs={12} md={6}>
+              <Button
+                className="button"
+                variant="contained"
+              >руки в экране есть!</Button>
+              <p>
+                {this.infoTexts[0]}
+              </p>
+            </Grid>
+            <Grid item xs={12} md={6}>
+              <Button
+                className="button"
+                variant="contained"
+              >рук в экране нет!</Button>
+              <p>
+                {this.infoTexts[1]}
+              </p>
+            </Grid>
+          </Grid>
+          <Grid container>
+            <Grid item md={12}>
+              <Button onClick={this.test} variant="contained">предсказать!</Button>
+            </Grid>
+          </Grid>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
