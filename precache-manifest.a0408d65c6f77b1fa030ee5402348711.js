3a6db25264b813162c716b09592299c653cc3d8b
Vterebenin <niceserver11@mail.ru>
2019-11-10 22:43:53 +0300

    Updates

---
+++ precache-manifest.a0408d65c6f77b1fa030ee5402348711.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e8d3d970f1ecc525b53870e5b1ccc9ee",
+    "url": "/learning-machine-learning/index.html"
+  },
+  {
+    "revision": "ca0375c9b069485d250c",
+    "url": "/learning-machine-learning/static/css/main.d5370121.chunk.css"
+  },
+  {
+    "revision": "3ca9bf6d7b4b4ed935a3",
+    "url": "/learning-machine-learning/static/js/2.1eb68e3c.chunk.js"
+  },
+  {
+    "revision": "ca0375c9b069485d250c",
+    "url": "/learning-machine-learning/static/js/main.c69d50ce.chunk.js"
+  },
+  {
+    "revision": "7a9e52d9c5985786d795",
+    "url": "/learning-machine-learning/static/js/runtime-main.72756407.js"
+  }
+]);
